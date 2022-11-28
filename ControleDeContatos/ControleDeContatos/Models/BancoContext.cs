a0000e2655573574a48ffa3534a3c89f5f2fcffc
joao neiva neto <joaoneivanetos@gmail.com>
2022-11-28 13:43:20 -0300

    first commit

---
+++ ControleDeContatos/ControleDeContatos/Models/BancoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Models
+{
+    public class BancoContext: DbContext
+    {
+        public readonly IConfiguration Configuration;
+
+        public BancoContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<ContatoMoldel> Contatos { get; set; }
+    }
+}
---
+++ ControleDeContatos/ControleDeContatos/Models/ContatoMoldel.cs
@@ -0,0 +1,15 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoMoldel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Celular { get; set; }
+
+
+    }
+}
